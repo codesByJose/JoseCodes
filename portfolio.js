d6089a533976496fc6deaaf55b6b5ca560e62c12
“joesthomas” <joesthomas2003@gmail.com>
2024-09-25 17:02:06 +0100

    next commits

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+const buttonCalculator = document.getElementById('button-calculator');
+const buttonTime = document.getElementById('button-time');
+const buttonFourCard = document.getElementById('button-four-card');
+
+buttonCalculator.addEventListener('click', ()=>{
+    window.open ('https://tip-master.netlify.app/', '_blank')
+});
+buttonTime.addEventListener('click', ()=>{
+    window.open ('https://jeremytimetrackingdashboard.netlify.app/', '_blank')
+});
+buttonFourCard.addEventListener('click', ()=>{
+    window.open ('https://fourfeaturecardsection.netlify.app/', '_blank')
+});
